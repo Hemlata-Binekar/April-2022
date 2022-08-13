376d77ba64d3f92efb9ce38ac345fa1f3e85a823
Hemlata-Binekar <hbinekar45@gmail.com>
2022-08-13 14:52:49 +0530

    updated dpkite file

--- TestNGPrograms/dpkite.java
+++ TestNGPrograms/dpkite.java
@@ -10,8 +10,10 @@ public class dpkite
 	  public Object[][] kiteData() {
 	    return new Object[][] {
 	      new Object[] { "XA0634", "1234568655", "122331"},
-	      new Object[] { "1234532", "12345743", "23111111"},
+	      new Object[] { "XA1532", "12345743", "23111111"},
 	      new Object[] { "XA0634", "Sarika699!", "691991"}
+	      new Object[] { "XA0453", "1765343", "111111"},
+	      new Object[] { "XA0211", "1765343", "10211111"},
 	    };
 	 }
 }
